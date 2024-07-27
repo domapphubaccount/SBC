d7142987db7bfa3f3e0a9bb9df848c2af9c3aab0
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-07-27 07:35:20 +0300

    fix style cards

--- src/components/dropDown/DropDown.jsx
+++ src/components/dropDown/DropDown.jsx
@@ -48,7 +48,7 @@ function DropDown({ userName }) {
             {dropDownToggle && (
                 <div
                     ref={dropdownRef}
-                    className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    className="archive_card  mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                     role="menu"
                     aria-orientation="vertical"
                     aria-labelledby="menu-button"
