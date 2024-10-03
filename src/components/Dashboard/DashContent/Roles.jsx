5d376655fd8123b6a21d5e9f977b008d6c321f17
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-10-03 21:13:28 +0300

    fix edit roles super admin

--- src/components/Dashboard/DashContent/Roles.jsx
+++ src/components/Dashboard/DashContent/Roles.jsx
@@ -289,6 +289,7 @@ function Roles({}) {
                           onClick={() => handlePermissionModule(item.id)}
                           type="button"
                           className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600"
+                          disabled={item.id == 1}
                         >
                           {item.permissions.length}
                         </button>
