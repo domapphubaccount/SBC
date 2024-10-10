6e478ef551e1f461cd38f383460148c3ef32b097
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-10-10 11:20:04 +0300

    fix roles search

--- src/components/Dashboard/DashContent/Roles.jsx
+++ src/components/Dashboard/DashContent/Roles.jsx
@@ -143,6 +143,11 @@ function Roles({}) {
     return `${year}-${month}-${day} At ${hours}:${minutes}`;
   }
 
+  // Step 3: Filter the rows based on the search term
+  const filteredData = rolesData.filter((item) => {
+    return item.name.toLowerCase().includes(searchTerm);
+  });
+
   return (
     <>
       <section>
@@ -254,8 +259,8 @@ function Roles({}) {
                 </tr>
               </thead>
               <tbody>
-                {rolesData.length > 0 ? (
-                  rolesData.map((item, index) => (
+                {rolesData && rolesData.length > 0 ? (
+                  filteredData.map((item, index) => (
                     <tr
                       key={index}
                       className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
@@ -336,11 +341,40 @@ function Roles({}) {
                           )}
                           {/* end view */}
                           {/* start edit */}
-                          {item.id !== 1 && permissionsData && permissionsData.includes(34) && (
-                            <Tooltip content="Edit Role">
+                          {item.id !== 1 &&
+                            permissionsData &&
+                            permissionsData.includes(34) && (
+                              <Tooltip content="Edit Role">
+                                <button
+                                  type="button"
+                                  className="flex items-center bg-slate-700 p-1 px-2 rounded text-white"
+                                >
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-4"
+                                    onClick={() => handleOpenEdit(item.id)}
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                                    />
+                                  </svg>
+                                </button>
+                              </Tooltip>
+                            )}
+                          {/* end edit */}
+                          {/* start delete */}
+                          {permissionsData && permissionsData.includes(35) && (
+                            <Tooltip content="Delete Role">
                               <button
                                 type="button"
-                                className="flex items-center bg-slate-700 p-1 px-2 rounded text-white"
+                                className="flex items-center bg-slate-700 p-1 px-2 rounded text-white "
+                                onClick={() => handleOpenDelete(item.id)}
                               >
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
@@ -349,42 +383,16 @@ function Roles({}) {
                                   strokeWidth={1.5}
                                   stroke="currentColor"
                                   className="size-4"
-                                  onClick={() => handleOpenEdit(item.id)}
                                 >
                                   <path
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
-                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                                   />
                                 </svg>
                               </button>
                             </Tooltip>
                           )}
-                          {/* end edit */}
-                          {/* start delete */}
-                          {permissionsData && permissionsData.includes(35) &&
-                          <Tooltip content="Delete Role">
-                            <button
-                              type="button"
-                              className="flex items-center bg-slate-700 p-1 px-2 rounded text-white "
-                              onClick={() => handleOpenDelete(item.id)}
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="size-4"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                />
-                              </svg>
-                            </button>
-                          </Tooltip>}
                           {/* start delete */}
                         </div>
                       </td>
