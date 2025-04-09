a4872e519eb3f4416a2b7a073ecad9e937372598
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2025-04-09 10:26:19 +0200

    update icons

--- src/components/Chat/mainChat/MainChat.jsx
+++ src/components/Chat/mainChat/MainChat.jsx
@@ -33,6 +33,7 @@ import {
   set_code_error,
 } from "@/app/Redux/Features/Code/CodeSlice";
 import { MathJax, MathJaxContext } from "better-react-mathjax";
+import "boxicons/css/boxicons.min.css";
 
 function MainChat({ windowWidth }) {
   const pathName = usePathname();
@@ -358,7 +359,7 @@ function MainChat({ windowWidth }) {
           </span>
 
           <div className="mt-4 text-xs">
-            <hr className="my-2"/>
+            <hr className="my-2" />
             📘 Reference: BYLD Documentation - Page 12
           </div>
         </MathJax>
@@ -394,7 +395,8 @@ function MainChat({ windowWidth }) {
                     style={{ height: "20px" }}
                   >
                     {!copyIcon ? (
-                      <svg
+                      <i
+                        class="bx bx-copy cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-200"
                         style={{ transition: "none" }}
                         onClick={(e) => {
                           handleCopyText(item.question, i);
@@ -408,19 +410,7 @@ function MainChat({ windowWidth }) {
                             }, 500);
                           }
                         }}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="black"
-                        className="size-3.5 mr-4 cursor-pointer"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M17.663 3.118c.225.015.45.032.673.05C19.876 3.298 21 4.604 21 6.109v9.642a3 3 0 0 1-3 3V16.5c0-5.922-4.576-10.775-10.384-11.217.324-1.132 1.3-2.01 2.548-2.114.224-.019.448-.036.673-.051A3 3 0 0 1 13.5 1.5H15a3 3 0 0 1 2.663 1.618ZM12 4.5A1.5 1.5 0 0 1 13.5 3H15a1.5 1.5 0 0 1 1.5 1.5H12Z"
-                          clipRule="evenodd"
-                        />
-                        <path d="M3 8.625c0-1.036.84-1.875 1.875-1.875h.375A3.75 3.75 0 0 1 9 10.5v1.875c0 1.036.84 1.875 1.875 1.875h1.875A3.75 3.75 0 0 1 16.5 18v2.625c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625v-12Z" />
-                        <path d="M10.5 10.5a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963 5.23 5.23 0 0 0-3.434-1.279h-1.875a.375.375 0 0 1-.375-.375V10.5Z" />
-                      </svg>
+                      ></i>
                     ) : (
                       i == copID && (
                         <svg
@@ -546,7 +536,7 @@ function MainChat({ windowWidth }) {
                     "Sorry there is an ERROR please try again"
                   ) ? (
                     <div className="flex mb-3">
-                      <svg
+                      <i
                         style={{ transition: "none" }}
                         onClick={(e) => {
                           handleReadText(item.answer, i); // Execute your copy function
@@ -562,14 +552,8 @@ function MainChat({ windowWidth }) {
                             }, 500); // Remove the class after 500ms
                           }
                         }}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="black"
-                        className="size-3.5 ml-2 cursor-pointer"
-                      >
-                        <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM18.584 5.106a.75.75 0 0 1 1.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 0 1-1.06-1.06 8.25 8.25 0 0 0 0-11.668.75.75 0 0 1 0-1.06Z" />
-                        <path d="M15.932 7.757a.75.75 0 0 1 1.061 0 6 6 0 0 1 0 8.486.75.75 0 0 1-1.06-1.061 4.5 4.5 0 0 0 0-6.364.75.75 0 0 1 0-1.06Z" />
-                      </svg>
+                        className="bx bx-volume-low cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-200"
+                      ></i>
 
                       {isSpeaking && i == copID && (
                         <svg
@@ -588,10 +572,11 @@ function MainChat({ windowWidth }) {
                       )}
 
                       {!copyIcon ? (
-                        <svg
+                        <i
+                          className="bx bx-copy ms-2 cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-200"
                           style={{ transition: "none" }}
                           onClick={(e) => {
-                            handleCopyText(item.answer, i); // Execute copy function
+                            handleCopyText(item.answer, i);
                             const svgElement = e.currentTarget; // Store the reference
                             if (svgElement) {
                               // Check if svgElement is not null
@@ -604,19 +589,7 @@ function MainChat({ windowWidth }) {
                               }, 500); // Remove the class after 500ms
                             }
                           }}
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="black"
-                          className="size-3.5 ml-2 cursor-pointer"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M17.663 3.118c.225.015.45.032.673.05C19.876 3.298 21 4.604 21 6.109v9.642a3 3 0 0 1-3 3V16.5c0-5.922-4.576-10.775-10.384-11.217.324-1.132 1.3-2.01 2.548-2.114.224-.019.448-.036.673-.051A3 3 0 0 1 13.5 1.5H15a3 3 0 0 1 2.663 1.618ZM12 4.5A1.5 1.5 0 0 1 13.5 3H15a1.5 1.5 0 0 1 1.5 1.5H12Z"
-                            clipRule="evenodd"
-                          />
-                          <path d="M3 8.625c0-1.036.84-1.875 1.875-1.875h.375A3.75 3.75 0 0 1 9 10.5v1.875c0 1.036.84 1.875 1.875 1.875h1.875A3.75 3.75 0 0 1 16.5 18v2.625c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625v-12Z" />
-                          <path d="M10.5 10.5a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963 5.23 5.23 0 0 0-3.434-1.279h-1.875a.375.375 0 0 1-.375-.375V10.5Z" />
-                        </svg>
+                        ></i>
                       ) : (
                         i == copID && (
                           <svg
@@ -638,7 +611,8 @@ function MainChat({ windowWidth }) {
                       {pathName.trim().slice(0, 9) !== "/sharable" &&
                         permissionsData &&
                         permissionsData.includes("openai.dislike_message") && (
-                          <svg
+                          <i
+                            class="bx bx-dislike ms-2 cursor-pointer hover:scale-105 active:scale-95 transition-transform duration-200"
                             style={{ transition: "none" }}
                             onClick={(e) => {
                               dislikeToggle(item.id);
@@ -685,19 +659,7 @@ function MainChat({ windowWidth }) {
                                 }, 500);
                               }
                             }}
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="black"
-                            className="size-3.5 ml-2 cursor-pointer"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-                            />
-                          </svg>
+                          ></i>
                         )}
                     </div>
                   ) : (
